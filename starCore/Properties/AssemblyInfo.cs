3b3d6b8e6b4d7307baab7334d85dda3ab9733479
steinsvik <espen@steinsvik.name>
2015-11-10 00:11:33 +0100

    Branding. Added "Steisnvik" as company in AssemblyInfo.cs.

--- starCore/Properties/AssemblyInfo.cs
+++ starCore/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("starCore")]
-[assembly: AssemblyDescription("Part of star tools and Resources. Basic utilities")]
+[assembly: AssemblyDescription("Part of Steinsvik Tools and Resources. Basic utilities.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Steinsvik")]
 [assembly: AssemblyProduct("starCore")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
